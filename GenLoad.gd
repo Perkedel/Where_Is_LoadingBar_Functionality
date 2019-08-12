c1b0749d31c8c99334d2da948338d96e553b23e8
Joel Robert Justiawan <joelwindows7@gmail.com>
2019-08-12 17:37:33 +0700

    QUIT button
    make the prototype legit serious demo for Game console Development Kit with this Quit button and display buttons

--- GenLoad.gd
+++ GenLoad.gd
@@ -2,7 +2,7 @@ extends Node
 
 # https://docs.godotengine.org/en/3.1/tutorials/io/background_loading.html Loading Thread, resource_queue.gd
 # https://godotengine.org/qa/8025/how-to-add-a-child-in-a-specific-position Spawn new child into a node out of instanced resource
-# https://www.youtube.com/watch?v=YMj2qPq9CP8 How to make Loading Bar in Unity
+# https://www.youtube.com/watch?v=YMj2qPq9CP8 (Not Godot, Unity instead, Inspiration) How to make Loading Bar with Loading Scene Coroutine in Unity
 # https://www.youtube.com/watch?v=9sHKaQBcgO8 (Unhelpful, No loading bar) Splash Screen Loadinger without bar
 # https://www.youtube.com/watch?v=-x0M17IwG0s (Unhelpful, crude box spawn method) Load some boxes into a scene
 
@@ -35,8 +35,23 @@ func _process(delta):
 		#print("ready")
 		pass
 	else:
+		ReadyToShow = false
 		LoadingValue = queue.get_progress("res://manies.tscn") * 100
 		#print("Unready")
+		pass
+	
+	if ReadyToShow:
+		$Control/Panel/ReadyToShowCheck.pressed = true
+		pass
+	else:
+		$Control/Panel/ReadyToShowCheck.pressed = false
+		pass
+	
+	if LoadSet:
+		$Control/Panel/LoadSetCheck.pressed = true
+		pass
+	else:
+		$Control/Panel/LoadSetCheck.pressed = false
 		pass
 	
 	$Control/ProgressBar.value = LoadingValue
@@ -50,20 +65,68 @@ func ShowLevel():
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		if ReadyToShow:
-			call_deferred("ShowLevel")
-			pass
+		SpawnResource()
+#		if ReadyToShow:
+#			call_deferred("ShowLevel")
+#			pass
 		pass
 	
 	if event.is_action_pressed("ui_down"):
-		queue.cancel_resource("res://manies.tscn")
+		CancelResource()
 		pass
 	
 	if event.is_action_pressed("ui_up"):
-		queue.queue_resource("res://manies.tscn",true)
+		QueueResource()
 		pass
 	
 	if event.is_action_pressed("ui_left"):
-		LoadSet = queue.get_resource("res://manies.tscn").instance()
+		InstanceResource()
 		pass
 	pass
+
+func QueueResource():
+	queue.queue_resource("res://manies.tscn",true)
+	pass
+func InstanceResource():
+	LoadSet = queue.get_resource("res://manies.tscn").instance()
+	pass
+func SpawnResource():
+	call_deferred("ShowLevel")
+	pass
+func CancelResource():
+	queue.cancel_resource("res://manies.tscn")
+	pass
+
+
+func _on_ButtonA_pressed():
+	QueueResource()
+	pass # Replace with function body.
+
+func _on_ButtonB_pressed():
+	InstanceResource()
+	pass # Replace with function body.
+
+func _on_ButtonC_pressed():
+	SpawnResource()
+	pass # Replace with function body.
+
+func _on_ButtonD_pressed():
+	CancelResource()
+	pass # Replace with function body.
+
+func _notification(what):
+	# https://docs.godotengine.org/en/3.1/tutorials/misc/handling_quit_requests.html
+	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
+		$Control/QuitConfirmationDialog.popup()
+		pass
+	pass
+	
+
+func _on_ConfirmationDialog_confirmed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_QUITButton_pressed():
+	$Control/QuitConfirmationDialog.popup()
+	pass # Replace with function body.
